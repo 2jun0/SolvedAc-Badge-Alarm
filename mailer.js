d5c67f669536343ad99ba695fbde6db1c2d88716
2jun0 <soo28819@gmail.com>
2023-04-11 00:40:34 +0900

    feat: update mailer

--- mailer.js
+++ mailer.js
@@ -3,12 +3,13 @@ require('dotenv').config()
 
 exports.sendEmail = async badge => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.SEND_ID,
       pass: process.env.SEND_PW,
-    },
-    secure: true
+    }
   })
 
   const to = [process.env.TO_ID]
